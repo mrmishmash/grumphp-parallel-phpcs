38287a3310944117c237066a9336adc00e0e4a89
mishmash <miha.wagner@gmail.com>
2024-10-19 18:58:18 +0200

    Use full classname when extending AbstractExternalTask.

--- src/Task/ParallelPhpCs.php
+++ src/Task/ParallelPhpCs.php
@@ -10,7 +10,6 @@ use GrumPHP\Fixer\Provider\FixableProcessResultProvider;
 use GrumPHP\Formatter\ProcessFormatterInterface;
 use GrumPHP\Process\TmpFileUsingProcessRunner;
 use GrumPHP\Runner\TaskResult;
-use GrumPHP\Task\AbstractExternalTask;
 use GrumPHP\Task\Config\ConfigOptionsResolver;
 use GrumPHP\Task\Context\ContextInterface;
 use GrumPHP\Task\Context\GitPreCommitContext;
@@ -24,9 +23,9 @@ use GrumPHP\Runner\TaskResultInterface;
 /**
  * Same as 'Phpcs' task in GrumPHP just that it adds parallel option.
  *
- * @extends AbstractExternalTask<ProcessFormatterInterface>
+ * @extends \GrumPHP\Task\AbstractExternalTask<ProcessFormatterInterface>
  */
-final class ParallelPhpCs extends AbstractExternalTask {
+final class ParallelPhpCs extends \GrumPHP\Task\AbstractExternalTask {
 
   protected $formatter;
 
